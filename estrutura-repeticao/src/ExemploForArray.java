3186ef4039a024ab2b44d6be8b1a5b12c22f7a7f
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2023-09-26 12:43:52 -0300

    forma abreviada do for

--- estrutura-repeticao/src/ExemploForArray.java
+++ estrutura-repeticao/src/ExemploForArray.java
@@ -2,10 +2,16 @@ public class ExemploForArray {
     public static void main(String[] args) {
         
         String alunos [] =  {"FELIPE","JONAS","JULIA","MARCOS"};
+
+        //forma abreviada
+        for(String aluno : alunos){
+            System.out.println("Nome do aluno é: " + alun);
+        }
 	
+        /* 
                                 //tamanho
         for(int x = 0; x < alunos.length; x++) {
             System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
-        }
+        }*/
     }
 }
